40da9f7511c8abf3c7f1e1db25e778f8867e43d9
BrianSayen <blu3fire89@gmail.com>
2023-03-30 09:02:25 +0800

    feat: udpate cors

--- server.js
+++ server.js
@@ -13,20 +13,22 @@ const { errorHandler } = require("./middlewares/errorHandler");
 const allowedOrigins = ["http://localhost:3000"];
 
 // * middlewares
-app.use(
-  cors({
-    origin(origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  }),
-);
+// app.use(
+//   cors({
+//     origin(origin, callback) {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         const msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   }),
+// );
+
+app.use(cors());
 app.use(express.json());
 
 connectDB();
